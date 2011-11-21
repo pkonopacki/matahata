c5261003603e1c50425fc92f3edc52866268203e
jez <jez@zwort.F-secure.com>
2011-11-21 00:31:06 +0000

    build and deployment scripts for debian

---
+++ b.sh
@@ -0,0 +1,11 @@
+cd matahata-core
+mvn clean package
+mvn install
+cd ..
+cd matahata-core-sp
+mvn clean package
+mvn install
+cd ..
+cd matahata-service
+mvn clean package
+cd ..
---
+++ d.sh
@@ -0,0 +1,3 @@
+sudo rm -r /var/lib/tomcat6/webapps/matahata-service
+sudo rm -r /var/lib/tomcat6/webapps/matahata-service.war
+sudo cp /home/jez/Matahata/matahata/matahata-service/target/matahata-service.war /var/lib/tomcat6/webapps/.
