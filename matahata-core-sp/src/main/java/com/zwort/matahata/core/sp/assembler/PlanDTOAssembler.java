dd0a14d43f610019a8f0cc911bcb9ab4588058a5
Piotr Konopacki <ext-piotr.konopacki@f-secure.com>
2011-11-22 01:38:40 +0200

    PlanItemDTO list to PlanDTO

--- matahata-core-sp/src/main/java/com/zwort/matahata/core/sp/assembler/PlanDTOAssembler.java
+++ matahata-core-sp/src/main/java/com/zwort/matahata/core/sp/assembler/PlanDTOAssembler.java
@@ -1,15 +1,43 @@
 package com.zwort.matahata.core.sp.assembler;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.zwort.matahata.core.model.Plan;
+import com.zwort.matahata.core.model.PlanItem;
 import com.zwort.matahata.core.sp.dto.PlanDTO;
+import com.zwort.matahata.core.sp.dto.PlanItemDTO;
 
 public class PlanDTOAssembler {
 	
 	public PlanDTO assemblePlanDto(Plan plan) {
 		PlanDTO dto = new PlanDTO();
+		
 		//TODO: Implement this assembler (po chuj?)
-//		dto.se
-//		dto.setEndDate(plan.getEndDate());
+		Set<PlanItemDTO> planItemDtoList = assemblePlanItemDtoList(plan.getPlanItemsList());
+		dto.getPlanItemDtoList().addAll(planItemDtoList);
+		dto.setStartDate(plan.getStartDate());
+		dto.setEndDate(plan.getStartDate());
+		dto.setId(plan.getId());
+		
+		return dto;
+	}
+
+	private Set<PlanItemDTO> assemblePlanItemDtoList(Set<PlanItem> planItemsList) {
+		Set<PlanItemDTO> planItemDtoList = new HashSet<PlanItemDTO>();
+		
+		for (PlanItem planItem : planItemsList) {
+			PlanItemDTO dto = assemblePlanItemDto(planItem);
+			planItemDtoList.add(dto);
+		}
+		
+		return planItemDtoList;
+	}
+
+	private PlanItemDTO assemblePlanItemDto(PlanItem planItem) {
+		PlanItemDTO dto = new PlanItemDTO();
+		dto.setCategoryAbbr(planItem.getCategory().getAbbreviation());
+		dto.setCatLimit(planItem.getAmount());
 		
 		return dto;
 	}
--- matahata-core-sp/src/main/java/com/zwort/matahata/core/sp/dto/PlanDTO.java
+++ matahata-core-sp/src/main/java/com/zwort/matahata/core/sp/dto/PlanDTO.java
@@ -1,5 +1,6 @@
 package com.zwort.matahata.core.sp.dto;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,6 +10,10 @@ public class PlanDTO extends BaseDTO {
 	
 	private Month month;
 	
+	private Date startDate;
+	
+	private Date endDate;
+	
 	private Set<PlanItemDTO> planItemDtoList;
 	
 	public Month getMonth() {
@@ -17,6 +22,22 @@ public class PlanDTO extends BaseDTO {
 
 	public void setMonth(Month month) {
 		this.month = month;
+	}
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
 	}
 
 	public Set<PlanItemDTO> getPlanItemDtoList() {
